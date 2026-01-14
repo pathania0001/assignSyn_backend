daf3d29226ff6c8c259f9ba2aba68c98dfe8d191
pathania0001 <83996978+pathania0001@users.noreply.github.com>
2026-01-14 22:46:06 +0530

    update url

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const config = require("./configs");
 
 const app = express();
 
-app.use(cors({ origin:config.ClientURL, credentials: true }));
+app.use(cors("*"));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
