a6bb3c05ec97ad8982dd258acd9649b38a066248
pathania0001 <83996978+pathania0001@users.noreply.github.com>
2026-01-14 22:29:41 +0530

    url add

--- configs/index.js
+++ configs/index.js
@@ -3,5 +3,6 @@ require("dotenv").config();
 module.exports = {
   PORT: process.env.PORT,
   API_PREFIX: process.env.API_PREFIX,
-  JSONPLACEHOLDER_BASE: process.env.JSONPLACEHOLDER_BASE
+  JSONPLACEHOLDER_BASE: process.env.JSONPLACEHOLDER_BASE,
+  ClientURL:process.env.ClientURL
 };
